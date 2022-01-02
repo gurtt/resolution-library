396c54471ce2f7e3fb5d7bb3d00b2d8b3f87a6c9
gurtt <6712553+gurtt@users.noreply.github.com>
2022-01-02 17:24:17 +1000

    add search function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+function search() {
+    let items = document.getElementById("items").getElementsByClassName("item");
+
+    for (const item of items) {
+        let itemName = item.getElementsByTagName("h2")[0].innerText;
+        if (itemName.toLowerCase().indexOf(document.getElementById("search").value.toLowerCase()) > -1) {
+            item.style.display = "";
+        } else {
+            item.style.display = "none";
+        }
+    }
+}
